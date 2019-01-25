7412647ad04f6ded29eb76d4ef41cdc1eb32f34d
Lo Saephan <teamsky916@gmail.com>
2019-01-25 12:23:24 -0800

    lick?

--- client/src/Squares.js
+++ client/src/Squares.js
@@ -243,7 +243,7 @@ class Squares extends Component {
 				- Must purchase square first to play. To purchase, venmo Kao Mouang at 'venmo' or Sing at 'venmo' or give cash to them.
 			</div>
 			<div>
-				Once payment is recieved, lick on the top right corner 'refresh' icon to update your square count, then you can begin playing.
+				- Once payment is recieved, tap on the top right corner 'refresh' icon to update your square count, then you can begin playing.
 			</div>
 			<div>
 				- The team numbers, shapes, colors and scoreboard your're seeing right now is for demonstration only and will be reset later.
